236ab35a252075def7222a18bcec46b0fe225001
akashkumar-404 <akashkumar404@gmail.com>
2022-07-21 00:10:47 +0530

    day1

---
+++ api/api.js
@@ -0,0 +1,8 @@
+fetch("https://api.cricapi.com/v1/currentMatches?apikey=609e0eac-7be1-4af5-90de-c5d770179806&offset=0")
+	.then(result => result.json())
+	.then(result => {
+		console.log("We have the result",result);
+	})
+	.catch(err => {
+		console.log("An error occured. Please check your code",err);
+	});
